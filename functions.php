90e502d626bb5f864e6796775613c9a44bd486d1
J3duardo <jesus_69eduardo@hotmail.com>
2020-05-28 17:06:51 -0400

    Functions: Widget personalizado para lista de categorías

--- functions.php
+++ functions.php
@@ -1,4 +1,6 @@
 <?php
+  require get_theme_file_path("/widgets/class-wp-widget-categories.php");
+
   add_action("after_setup_theme", "theme_setup");
   function theme_setup() {
     // Habilitar featured images
@@ -8,4 +10,19 @@
 
     // Especificar post formats
     add_theme_support("post-formats", array("gallery"));
+  }
+
+  // Habilitar widgets locations
+  add_action("widgets_init", "init_widgets");
+  function init_widgets($id) {
+    register_sidebar(array(
+      "name" => "Sidebar",
+      "id" => "sidebar",
+    ));
+  }
+
+  // Registrar widgets
+  add_action("widgets_init", "custom_widgets_register");
+  function custom_widgets_register() {
+    register_widget("WP_Widget_Categories_Custom");
   }
